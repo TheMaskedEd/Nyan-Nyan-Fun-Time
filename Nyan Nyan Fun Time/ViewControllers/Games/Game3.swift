11b070472d021d03e42cc4ebfdb1adbdf28a9e62
Edward Goodall <ecgoodall@me.com>
2018-06-20 18:43:27 +0100

    setup sprite kit and the view controllers again

---
+++ Nyan Nyan Fun Time/ViewControllers/Games/Game3.swift
@@ -0,0 +1,36 @@
+//
+//  Jobs.swift
+//  Nyan Nyan Fun Time
+//
+//  Created by Edward Goodall on 6/20/18.
+//  Copyright © 2018 Kofu Interactive. All rights reserved.
+//
+//  dont delete any of this code it is crucial for spritekit
+
+import UIKit
+import SpriteKit
+
+class Game3: UIViewController {
+    
+    let sKView: SKView = {
+        let view = SKView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(sKView)
+        sKView.topAnchor .constraint(equalTo: view.topAnchor).isActive = true
+        sKView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        sKView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        sKView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
