b6b80468a8c5292f02158e2fd4de48b3bf8bed3a
Edward Goodall <ecgoodall@me.com>
2018-09-25 16:09:55 -0500

    made changes

--- Nyan Nyan Fun Time/ViewControllers/Chore.swift
+++ Nyan Nyan Fun Time/ViewControllers/Chore.swift
@@ -12,68 +12,116 @@ import UIKit
 //add their vaules onto status
 
 //update 1
-var health:Bool? = nil
-var happiness:Bool? = nil
-var energy:Bool? = nil
-var hygiene:Bool? = nil
-var hunger:Bool? = nil
-var pain:Bool? = nil
+var activated:[Int] = []
 class Chore: UIViewController {
     @IBOutlet weak var imageChore: UIImageView!
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        func image(image1:UIImage,image2:UIImage,image3:UIImage,image4:UIImage){
-            let imageNames = [image1,image2,image3,image4]
-        
-            imageChore.animationImages = imageNames
-            imageChore.animationDuration = 2.0
-            imageChore.startAnimating()
-       }
+    var image1:UIImage? = nil
+    var image2:UIImage? = nil
+    var image3:UIImage? = nil
+    var image4:UIImage? = nil
+    @IBAction func pressHere(_ sender: UIButton) {
         switch inventoryActive {
         case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
             print("ItemSelected0")
-        case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
+        case ["Good Food"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+           activated = [0,1,2,3,4,5]
             print("ItemSelected1")
-         case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
+        case ["Best Food"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
             print("ItemSelected2")
-         case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
+        case ["Good Bed"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
             print("ItemSelected3")
-         case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
+        case ["Best Bed"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
             print("ItemSelected4")
-        case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
-            print("ItemSelected0")
-        case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
-            print("ItemSelected1")
-        case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
-            print("ItemSelected2")
-        case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
-            print("ItemSelected3")
-        case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
-            print("ItemSelected4")
-        case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
-            print("ItemSelected3")
-        case ["Basic Food"]:
-            image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
-            print("ItemSelected4")
+        case ["Basic shampoo"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
+            print("ItemSelected5")
+        case ["Good shampoo"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
+            print("ItemSelected6")
+        case [ "Best shampoo"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
+            print("ItemSelected7")
+        case ["Good Brush"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
+            print("ItemSelected8")
+        case ["Best Brush"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
+            print("ItemSelected9")
+        case ["Good Nail"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
+            print("ItemSelected10")
+        case ["Best Nail"]:
+            image1 = #imageLiteral(resourceName: "ONE")
+            image2 = #imageLiteral(resourceName: "TWO")
+            image3 = #imageLiteral(resourceName: "three")
+            image4 = #imageLiteral(resourceName: "four")
+            activated = [0,1,2,3,4,5]
+            print("ItemSelected11")
+            
         default:
-        print("passthrough images not displayed")
-        image(image1:#imageLiteral(resourceName: "ONE"),image2:#imageLiteral(resourceName: "TWO"),image3:#imageLiteral(resourceName: "three"),image4:#imageLiteral(resourceName: "four") )
+            print("noimageselected")
         }
-}
+    
+            let imageNames = [image1,image2,image3,image4]
+            imageChore.animationImages = imageNames as? [UIImage]
+            imageChore.animationDuration = 2.0
+            imageChore.startAnimating()
+    
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    
+        }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
  }
@@ -83,3 +131,5 @@ class Chore: UIViewController {
 
 
 }
+
+
--- Nyan Nyan Fun Time/ViewControllers/Status.swift
+++ Nyan Nyan Fun Time/ViewControllers/Status.swift
@@ -11,6 +11,7 @@ import UIKit
 // use the function used in chore and use it to add values
 // also use a value to decrease progreesbars by .1 every 30 seconds
 
+
 class Status: UIViewController {
     @IBOutlet var health: UIProgressView!
     
@@ -24,40 +25,65 @@ class Status: UIViewController {
    
     @IBOutlet var hunger: UIProgressView!
     
-
-    let total = 1
-    var increase = 0
-    var decrease:Double = 10
-    @objc func counter() {
-        self.health.progress = (Float)(decrease/10)
-        self.happiness.progress = (Float)(decrease/10)
-        self.energy.progress = (Float)(decrease/10)
-        self.hygiene.progress = (Float)(decrease/10)
-        self.hunger.progress = (Float)(decrease/10)
-        decrease -= 0.05
+    lazy var healthPG = health.progress
+    lazy var happinessPG = happiness.progress
+    lazy var energyPG = energy.progress
+    lazy var hygienePG = hygiene.progress
+    lazy var painPG = pain.progress
+    lazy var hungerPG = hunger.progress
+    
+    
+    var Change:Double = 10
+    @objc func counter0() {
+        self.health.progress = (Float)(Change/10)
+        self.happiness.progress = (Float)(Change/10)
+        self.energy.progress = (Float)(Change/10)
+        self.hygiene.progress = (Float)(Change/10)
+        self.hunger.progress = (Float)(Change/10)
+        Change -= 0.05
     }
-   if pain == true {
-   self.pain.progress = (increase/1)
-   increase += .25
-   }
-    if  health == true{
-    
-    
-    
+  
+    @objc func counter1() {
+        Change += 0.1
+    }
+
     
     var timer:Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:#selector(counter), userInfo: nil, repeats:true)
+        if activated == [1]{
+           for i
+            self.health.progress = (Float)(Change)
+        }
+        if activated == [2]{
+            self.happiness.progress = (Float)(Change)
+        }
+        if activated == [3]{
+            self.energy.progress = (Float)(Change)
+        }
+        if activated == [4]{
+            self.hygiene.progress = (Float)(Change)
+        }
+        if activated == [5]{
+            self.hunger.progress = (Float)(Change)
+        }
+        if activated == [6]{
+            self.pain.progress = (Float)(Change)
+        }
         
-        hunger.setProgress(Float(total), animated: true)
-        happiness.setProgress(Float(total), animated: true)
-        energy.setProgress(Float(total), animated: true)
-        hygiene.setProgress(Float(total), animated: true)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:#selector(counter0), userInfo: nil, repeats:true)
+        
+        
+        hunger.setProgress(Float(hungerPG), animated: true)
+        happiness.setProgress(Float(happinessPG), animated: true)
+        energy.setProgress(Float(energyPG), animated: true)
+        hygiene.setProgress(Float(hygienePG), animated: true)
         pain.setProgress(Float(0), animated: true)
-        health.setProgress(Float(total), animated: true)
+        health.setProgress(Float(healthPG), animated: true)
+        
     }
     
 
@@ -65,6 +91,7 @@ class Status: UIViewController {
         super.didReceiveMemoryWarning()
     }
 }
+
 
 
 
